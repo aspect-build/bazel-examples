06a28fb866416e78e9c4c09cf5c1e291256be93e
Alex Eagle <alex@aspect.dev>
2022-06-02 06:56:10 -0700

    Copy over most tsx code from example

---
+++ next.js/components/post-body.tsx
@@ -0,0 +1,20 @@
+// needed to provide typings for *.module.css
+/// <reference types="next" />
+import markdownStyles from './markdown-styles.module.css'
+
+type Props = {
+  content: string
+}
+
+const PostBody = ({ content }: Props) => {
+  return (
+    <div className="max-w-2xl mx-auto">
+      <div
+        className={markdownStyles['markdown']}
+        dangerouslySetInnerHTML={{ __html: content }}
+      />
+    </div>
+  )
+}
+
+export default PostBody
