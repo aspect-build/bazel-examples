470f1d1d20fd1dd6bac4b3a874f6f8d38ee9c740
Jason Bedard <jason+github@jbedard.ca>
2022-06-22 10:21:35 -0700

    example(angular): initial "ng new" project
    Example created using Angular CLI v14.
    
    Project created using `ng new`.
    Project libraries 'lib-a' and 'common' created using `ng add project`.
    Project and library e2e tests and service examples removed for simplicity.
    
    Application depends on 'lib-a', 'lib-a' depends on 'common'.

---
+++ angular/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { LibAModule } from 'lib-a';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    LibAModule
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
