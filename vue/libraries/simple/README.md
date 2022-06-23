fdd1ec7622609bc4c60edd54dc4c8f8e8e196937
Alex Eagle <alex@aspect.dev>
2022-06-23 08:25:14 -0700

    vue: add a component library to show how Bazel can build a dependency graph (#28)

---
+++ vue/libraries/simple/README.md
@@ -0,0 +1,5 @@
+# Simple Vue Component Library
+
+This shows how you can build libraries in a monorepo with Bazel.
+
+Example code is from https://blog.logrocket.com/building-vue-3-component-library/
---
+++ vue/libraries/simple/rollup.config.js
@@ -0,0 +1,10 @@
+import vue from 'rollup-plugin-vue'
+import peerDepsExternal from 'rollup-plugin-peer-deps-external'
+
+export default [
+  {
+    plugins: [
+      vue(), peerDepsExternal()
+    ]
+  }
+]
