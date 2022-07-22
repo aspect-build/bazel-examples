6a22817422256567ee1f830b7b35e7c24062c091
Jason Bedard <jason+github@jbedard.ca>
2022-07-22 14:26:14 -0700

    ci: add bazel test ci to each example

--- check-npm-determinism/test.sh
+++ check-npm-determinism/test.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -o errexit -o nounset -o pipefail
 
+# Check NPM determinism
+
 script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd "$script_dir"
 
