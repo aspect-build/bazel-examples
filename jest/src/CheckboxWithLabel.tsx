c355c701ff3a1de66085595db6fff77ce409702d
Greg Magolan <gmagolan@gmail.com>
2022-10-14 16:45:48 -0700

    Add example of using jest_test with ts_project & swc

---
+++ jest/src/CheckboxWithLabel.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+
+export default function CheckboxWithLabel({ labelOn, labelOff }) {
+  const [isChecked, setIsChecked] = useState(false);
+
+  const onChange = () => {
+    setIsChecked(!isChecked);
+  };
+
+  return (
+    <label>
+      <input type="checkbox" checked={isChecked} onChange={onChange} />
+      {isChecked ? labelOn : labelOff}
+    </label>
+  );
+}
