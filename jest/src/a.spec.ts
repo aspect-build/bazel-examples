4cc755e25abaa4c2042c8a6502166e3523516713
Greg Magolan <gmagolan@gmail.com>
2022-10-16 15:49:12 -0700

    Repro of @open-wc/dedupe-mixin issue

--- jest/src/a.spec.ts
+++ jest/src/a.spec.ts
@@ -1,5 +1,11 @@
 import { name } from './a';
+import { fixture } from '@open-wc/testing-helpers';
 
-test('a', () => {
+it('can instantiate an element', async () => {
+  const el = await fixture('<my-el foo="bar"></my-el>');
+  expect(el.getAttribute('foo')).toEqual('bar');
+});
+
+it('a', () => {
   expect(name()).toBe('a');
 });
