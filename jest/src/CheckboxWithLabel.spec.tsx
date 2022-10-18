94d50cbae488fccf3941e42ea3a05324018ecb83
Greg Magolan <gmagolan@gmail.com>
2022-10-17 19:59:23 -0700

    Add @testing-library/jest-dom to jest example (#116)

--- jest/src/CheckboxWithLabel.spec.tsx
+++ jest/src/CheckboxWithLabel.spec.tsx
@@ -1,10 +1,13 @@
-import { fireEvent, render } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import CheckboxWithLabel from './CheckboxWithLabel';
+import '@testing-library/jest-dom/extend-expect';
 
 it('CheckboxWithLabel changes the text after click', () => {
   const { queryByLabelText, getByLabelText } = render(
     <CheckboxWithLabel labelOn="On" labelOff="Off" />
   );
+
+  expect(screen.getByText(/off/i)).toBeInTheDocument();
 
   expect(queryByLabelText(/off/i)).toBeTruthy();
 
