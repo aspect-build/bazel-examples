ed765e1909e787c3a6453f38211077b8206d2a50
Jim Brännlund <jim.brannlund@gmail.com>
2022-10-19 13:05:08 +0200

    add transitive 1st and 3rd-party deps

---
+++ pnpm-workspaces/packages/first/src/main.js
@@ -0,0 +1,3 @@
+module.exports = first = () => {
+  console.log('I am First!');
+};
--- pnpm-workspaces/packages/two/src/main.js
+++ pnpm-workspaces/packages/two/src/main.js
@@ -1,3 +1,14 @@
+const cowsay = require('cowsay');
+const First = require('@bazel-poc/first');
+
 module.exports = two = () => {
+  First();
   console.log('I am Two, not One!');
+  console.log(
+    cowsay.say({
+      text: "I'm a transitive moooodule",
+      e: 'oO',
+      T: 'U ',
+    })
+  );
 };
