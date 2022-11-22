c733adfd3ca8caa556c92cf5788f7c81be5f14dd
Greg Magolan <gmagolan@gmail.com>
2022-11-21 22:15:36 -0800

    Upgrade react-cra example to use ts_project, jest_test & js_run_devserver

--- react-cra/src/App.test.tsx
+++ react-cra/src/App.test.tsx
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import '@testing-library/jest-dom/extend-expect';
 
 test('renders learn react link', () => {
   render(<App />);
