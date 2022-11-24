83307c60f45f19174a8b1b261787214ff48fd888
Greg Magolan <gmagolan@gmail.com>
2022-11-23 18:29:21 -0800

    chore: add some comments explaining the @testing-library/jest-dom/extend-expect imports in jest & react-cra examples

--- react-cra/src/App.test.tsx
+++ react-cra/src/App.test.tsx
@@ -1,5 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+
+// Used for __tests__/testing-library.js
+// Learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
 
 test('renders learn react link', () => {
