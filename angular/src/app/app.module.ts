2300e7d2b560533015833ae1b002b2db4881d112
Jack Vincent <50340752+jackvincentnz@users.noreply.github.com>
2023-03-24 17:20:16 -0700

    example(angular): add custom_postinstall ngcc example

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { DragulaModule } from 'ng2-dragula';
 
 import { LibAModule } from 'lib-a';
 
 import { AppComponent } from './app.component';
+import { DragulaComponent } from './dragula.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, LibAModule],
+  declarations: [AppComponent, DragulaComponent],
+  imports: [BrowserModule, LibAModule, DragulaModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
