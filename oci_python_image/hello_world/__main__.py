8403bba30e742372e555ad4a50e5007b8b91a199
Thulio Ferraz Assis <3149049+f0rmiga@users.noreply.github.com>
2023-05-11 10:04:01 -0700

    feat: example for python integration with rules_oci
    Signed-off-by: Thulio Ferraz Assis <3149049+f0rmiga@users.noreply.github.com>

---
+++ oci_python_image/hello_world/__main__.py
@@ -0,0 +1,5 @@
+from hello_world.app import Helloworld
+
+if __name__ == "__main__":
+    app = Helloworld("John")
+    app.say_hello()
---
+++ oci_python_image/hello_world/app.py
@@ -0,0 +1,6 @@
+class Helloworld:
+    def __init__(self, name):
+        self.name = name
+
+    def say_hello(self):
+        print(f"Hello {self.name}!")
