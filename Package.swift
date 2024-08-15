812ab24ccb03e008f4ac2f08ffe4a2d82f79ffcf
Alex Eagle <alex@aspect.dev>
2024-08-15 14:38:14 -0700

    chore: add root workspace setup

---
+++ Package.swift
@@ -0,0 +1,10 @@
+// swift-tools-version: 5.7
+
+import PackageDescription
+
+let package = Package(
+    name: "aspect-bazel-examples",
+    dependencies: [
+        .package(url: "https://github.com/SwifterSwift/SwifterSwift.git", from: "5.3.0"),
+    ]
+)
