b46f18c2d820f9f9a6e07c45301bd8f59e55b309
Kyle Cordes <kyle@kylecordes.com>
2024-08-29 09:38:25 -0400

    Attempt to include C++ 'exercises' into CI

---
+++ test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Run the overall tests and then run the Speller-specific C++ testing.
+
+bazel \
+--bazelrc=$GITHUB_WORKSPACE/.aspect/bazelrc/ci.bazelrc \
+--bazelrc=$GITHUB_WORKSPACE/.github/workflows/ci.bazelrc \
+test //...
+
+cd speller
+./exercise.sh
