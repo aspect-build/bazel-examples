9d5f1970d7e6cfa2f9df8c03faf78e8a9cac0564
Alex Eagle <alex@aspect.dev>
2024-08-31 14:38:34 -0700

    refactor: simplify

--- tools/_multitool_run_under_cwd.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# See https://blog.aspect.build/run-tools-installed-by-bazel
-target="@multitool//tools/$(basename "$0")"
-# NB: we don't use 'bazel run' because it may leave behind zombie processes under ibazel
-bazel 2>/dev/null build "$target" && exec $(bazel info execution_root)/$(bazel 2>/dev/null cquery --output=files "$target") "$@"
