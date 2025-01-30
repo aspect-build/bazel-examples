10cb91612cfb29f94b768806e9f0526e4cac913b
Alex Eagle <alex@aspect.dev>
2025-01-30 14:29:25 -0800

    did I break this test?

--- oci_go_image/main_test.go
+++ oci_go_image/main_test.go
@@ -8,7 +8,7 @@ import (
 func TestCompare(t *testing.T) {
 	result := Compare("this", "that")
 
-	if !strings.Contains(result, "this") {
+	if !strings.Contains(result, "other") {
 		t.Error("expected a diff containing 'this' but got", result)
 	}
 }
