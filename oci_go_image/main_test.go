fc856489d51ebb0fa5b87914a5c17836d35a1826
Alex Eagle <alex@aspect.dev>
2025-01-30 14:32:47 -0800

    I think I broke this test

--- oci_go_image/main_test.go
+++ oci_go_image/main_test.go
@@ -8,7 +8,7 @@ import (
 func TestCompare(t *testing.T) {
 	result := Compare("this", "that")
 
-	if !strings.Contains(result, "this") {
+	if !strings.Contains(result, "wrong") {
 		t.Error("expected a diff containing 'this' but got", result)
 	}
 }
