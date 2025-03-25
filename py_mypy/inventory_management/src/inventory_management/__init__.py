aa589160fdc2c571a08bb8935e48da8121cfec14
Reid D. McKenzie <reid@aspect.build>
2025-03-25 14:35:08 -0600

    Attempting to replicate rules_mypy#74

---
+++ py_mypy/inventory_management/src/inventory_management/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+from inventory_management.tool import adjust_stock
+
+__all__ = ["adjust_stock"]
--- py_mypy/inventory_management/src/inventory_management/tool.py
+++ py_mypy/inventory_management/src/inventory_management/tool.py
@@ -1,5 +1,10 @@
+# Note that this file is both a runnable script and somewhat used as library code.
+# This is poor style, but can happen in the real world.
+
 from data_models import Product
 from data_persistence import update_stock
+
+import click
 
 def check_stock(product_id: int) -> int:
     #Simulating stock retrieval. In real code this would be from data_persistence.
@@ -13,3 +18,15 @@ def adjust_stock(product_id: int, quantity_change: int):
 
     update_stock(product_id, new_stock)
     print(f"Adjusted stock for product {product_id} to {new_stock}")
+
+
+@click.group()
+def cli():
+    pass
+
+@cli.command("adjust")
+def cmd_adjust():
+    pass
+
+if __name__ == "__main__":
+    cli()
