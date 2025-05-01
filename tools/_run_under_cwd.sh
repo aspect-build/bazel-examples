1125cc29eca28d842112345520bc40dc70c0b072
Alex Eagle <alex@aspect.dev>
2025-04-30 20:58:30 -0700

    refactor: try bazel_env.bzl
    Has a bit more install steps, but a nicer DX when the tools are on the path

--- tools/_run_under_cwd.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# See https://blog.aspect.build/run-tools-installed-by-bazel
-target="//tools:$(basename "$0")"
-
-# NB: we don't use 'bazel run' because it may leave behind zombie processes under ibazel
-# shellcheck disable=SC2046
-bazel 2>/dev/null build --build_runfile_links "$target" && \
-  BAZEL_BINDIR=. exec $(bazel 2>/dev/null info execution_root)/$(bazel 2>/dev/null cquery --output=files "$target") "$@"
