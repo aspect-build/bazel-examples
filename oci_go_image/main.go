a73c6e7c26758effbce295800dd495007a02ebee
Alex Eagle <alex@aspect.dev>
2025-05-23 16:44:10 -0700

    Update main.go

--- oci_go_image/main.go
+++ oci_go_image/main.go
@@ -11,5 +11,6 @@ func Compare(str1, str2 string) string {
 }
 
 func main() {
-	fmt.Println(Compare("Hello World change", "Hello Go"))
+	// this comment
+	fmt.Println(Compare("Hello World", "Hello Go"))
 }
