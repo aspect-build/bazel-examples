cddc3ef654c84209f33d1e988c4030c4c8c778f6
Alex Eagle <alex@aspect.build>
2025-09-04 14:43:19 -0700

    feat: demonstrate Node-API for native interop (#468)
    * feat: demonstrate Node-API for native interop
    
    * Apply suggestions from code review
    
    Co-authored-by: aspect-workflows[bot] <143031405+aspect-workflows[bot]@users.noreply.github.com>
    
    * fix gazelle
    
    ---------
    
    Co-authored-by: aspect-workflows[bot] <143031405+aspect-workflows[bot]@users.noreply.github.com>

--- nodejs_apps/typescript/src/main.ts
+++ nodejs_apps/typescript/src/main.ts
@@ -1,6 +1,7 @@
 import { platform, arch } from 'node:os';
 import { one } from '@bazel-examples/one';
 import { shared } from '@bazel-examples/shared';
+import { rust, c } from '@bazel-examples/napi';
 import { getRandomQuote } from 'inspirational-quotes';
 import * as quotes from 'star-wars-quotes';
 
@@ -8,5 +9,7 @@ shared();
 one();
 console.log(getRandomQuote());
 console.log(quotes());
+console.log(rust.hello('NodeJS'));
+console.log('C code does math: 1 + 2 = ' + c.add(1, 2));
 console.log('Platform: ' + platform());
 console.log('Architecture: ' + arch());
---
+++ npm_packages/napi/src/adder.c
@@ -0,0 +1,5 @@
+#include "adder.h"
+
+int add(int first, int second) {
+    return first + second;
+}
