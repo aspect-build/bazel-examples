96e540ed1a6035adb6fcf3afc708dfbb909b1748
Alex Eagle <alex@aspect.build>
2025-10-15 14:50:05 -0700

    Update index.ts

--- logger/frontend/index.ts
+++ logger/frontend/index.ts
@@ -2,7 +2,8 @@ import type { LogMessage } from '../schema/logger_pb';
 
 class ServerLogs {
   getServerLogs(): Promise<LogMessage[] | undefined> {
-    return fetch('http://localhost:8081')
+    let port: string = 8081
+    return fetch(`http://localhost:{}`)
       .then((response) => this.checkStatus(response))
       .then((response) =>
         this.parseJSON(response)
