eeafb0b9b0ee3bf31c3caf8ca8f325175c519758
Alex Eagle <alex@aspect.build>
2025-12-09 08:25:10 -0800

    Update log message in JavaLoggingClientLibraryTest

--- logger/client/src/build/aspect/JavaLoggingClientLibraryTest.java
+++ logger/client/src/build/aspect/JavaLoggingClientLibraryTest.java
@@ -23,7 +23,7 @@ public final class JavaLoggingClientLibraryTest {
 
   @Test
   public void testSendLogMessageToServer() throws Exception {
-    client.sendLogMessageToServer("he3llo");
+    client.sendLogMessageToServer("he23llo");
   }
 
 
