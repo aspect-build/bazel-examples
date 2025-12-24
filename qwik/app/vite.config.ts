7ed1a22bf7f1d705cb7400ff3d98bd0705322288
Alex Eagle <alex@aspect.dev>
2025-12-24 14:07:41 -0800

    undo qwik change by agent

--- qwik/app/vite.config.ts
+++ qwik/app/vite.config.ts
@@ -42,11 +42,6 @@ export default defineConfig(({ command, mode }): UserConfig => {
       tsconfigPaths(),
       watchNodeModules(['my-qwik-library-name']),
     ],
-    resolve: {
-      alias: {
-        '@builder.io/qwik/preloader': '@builder.io/qwik/core/preloader',
-      },
-    },
     // This tells Vite which dependencies to pre-build in dev mode.
     optimizeDeps: {
       // Put problematic deps that break bundling here, mostly those with binaries.
