4f50918f357e6413eb7e3ae3b4fbc95225a451f6
Matt Cramer <b3cramer@gmail.com>
2026-05-04 09:58:09 -0600

    Fix test to check for 'this' instead of 'thisdemo'

--- oci_go_image/main_test.go
+++ oci_go_image/main_test.go
@@ -8,7 +8,7 @@ import (
 func TestCompare(t *testing.T) {
 	result := Compare("this", "that")
 
-	if !strings.Contains(result, "thisdemo") {
+	if !strings.Contains(result, "this") {
 		t.Error("expected a diff containing 'this' but got", result)
 	}
 }
