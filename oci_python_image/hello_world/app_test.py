38e3acbd88e75b5e8eb771d97d6136442fd65dcc
Jeff Pignataro <jeff@aspect.build>
2026-05-04 08:31:54 -0600

    feat: add linting configuration and update test file comments

--- oci_python_image/hello_world/app_test.py
+++ oci_python_image/hello_world/app_test.py
@@ -4,3 +4,8 @@ from hello_world.app import Cow
 def test_moo():
     app = Cow("John")
     app.say_hello()
+
+# Deliberately failing test to demonstrate test failure
+# # def test_name2():
+#     app = Cow("John")
+#     assert app.name == "James"
