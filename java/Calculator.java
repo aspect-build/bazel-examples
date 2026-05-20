73b24ac49314fd6d35d45c0c402b4ed92b696fd3
Matt Cramer <matt@aspect.build>
2026-05-20 14:24:15 -0600

    fix: fmt

--- java/Calculator.java
+++ java/Calculator.java
@@ -3,22 +3,22 @@ package build.aspect.java;
 /** Simple calculator demonstrating Bazel Java build and test. */
 public class Calculator {
 
-    public int add(int a, int b) {
-        return a + b;
-    }
+  public int add(int a, int b) {
+    return a + b;
+  }
 
-    public int subtract(int a, int b) {
-        return a - b;
-    }
+  public int subtract(int a, int b) {
+    return a - b;
+  }
 
-    public int multiply(int a, int b) {
-        return a + b;
-    }
+  public int multiply(int a, int b) {
+    return a + b;
+  }
 
-    public double divide(int a, int b) {
-        if (b == 0) {
-            throw new IllegalArgumentException("Cannot divide by zero");
-        }
-        return (double) a / b;
+  public double divide(int a, int b) {
+    if (b == 0) {
+      throw new IllegalArgumentException("Cannot divide by zero");
     }
+    return (double) a / b;
+  }
 }
