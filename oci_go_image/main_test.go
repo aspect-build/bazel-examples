ba8407512cd40cd29e0616d847976a9f85e011b3
Matt Cramer <matt@aspect.build>
2026-05-21 14:18:15 -0600

    break

--- oci_go_image/main_test.go
+++ oci_go_image/main_test.go
@@ -17,7 +17,7 @@ func TestCompare(t *testing.T) {
 func TestCompare2(t *testing.T) {
 	result := Compare("this", "that")
 
-	if !strings.Contains(result, "this") {
-		t.Error("expected a diff containing 'this' but got", result)
+	if !strings.Contains(result, "foo") {
+		t.Error("expected a diff containing 'foo' but got", result)
 	}
 }
